49d8193c6885768e84eee1f11095c35902530e0e
akiyamiyamoto <akiya.miyamoto@kek.jp>
2019-01-25 13:46:38 +0900

    Add path for cmake

--- geant4/LC/LC1/setup-cvmfs.sh
+++ geant4/LC/LC1/setup-cvmfs.sh
@@ -6,6 +6,8 @@
 export PATH=/cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9.3/x86_64-slc6/bin:/cvmfs/sft.cern.ch/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/bin:${PATH}
 export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9.3/x86_64-slc6/lib:/cvmfs/sft.cern.ch/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/lib:${LD_LIBRARY_PATH}
 
+# Setup cmake
+export PATH="/cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/CMake/3.6.3/bin:$PATH"
 export CXX=g++
 export CC=gcc
  
