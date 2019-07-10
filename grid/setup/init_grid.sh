f1d3c1217b7e8e9df7cbddd05b29d1cb7b1b5e92
akiyamiyamoto <akiya.miyamoto@kek.jp>
2019-07-10 15:18:48 +0900

    glite_cream.conf is created automatically, if not exist. jobid.txt is removed.

--- grid/setup/init_grid.sh
+++ grid/setup/init_grid.sh
@@ -1,4 +1,14 @@
 
+if [ ! -e ~/.glite/glite_cream.conf ] ; then 
+    echo "### ~/.glite/glite_cream.conf does not exist. It is created now."
+    mkdir -vp ~/.glite
+    echo "[" > ~/.glite/glite_cream.conf
+    echo " UBERFTP_CLIENT=\"/cvmfs/grid.cern.ch/emi3ui-latest/usr/bin/uberftp\"" >> ~/.glite/glite_cream.conf
+    echo "]" >> ~/.glite/glite_cream.conf
+    echo "~/.glite/glite_cream.conf was created. "
+    echo ""
+fi
+
 source ./setup-cvmfs-ui.sh 
 voms-proxy-init --voms ilc -valid 144:00 -vomslife 144:00
 
