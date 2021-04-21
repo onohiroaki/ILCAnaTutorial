df7d063aa145b743f1bd1413269b85615687603a
akiyamiyamoto <akiya.miyamoto@kek.jp>
2021-04-21 19:22:54 +0900

    Updated for build on new CentOS7

--- PartOne/tools/centos7/build-base.sh
+++ PartOne/tools/centos7/build-base.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 
 ## Install root, lcio, and dependant software
+# 
+# Install required packages by 
+#  sudo yum install  mysql mariadb-devel libXpm-devel libXft-devel libXext-devel doxygen
+#  
 
 # Get the latest ILCSoft
 wget https://github.com/iLCSoft/iLCInstall/archive/v02-02-01.tar.gz
@@ -18,7 +22,7 @@ export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_
 export CXX=g++
 export CC=gcc
 
-ncores=4    # Number of cores to use for build
+ncores=8    # Number of cores to use for build
 ./ilcsoft-install -j ${ncores} --install-prefix=${HOME}/Tutorial/sw/x86_64_gcc82_centos7 \
     -i partone-base.cfg 
 
--- PartOne/tools/centos7/build-whizard2.sh
+++ PartOne/tools/centos7/build-whizard2.sh
@@ -4,9 +4,9 @@ echo "Build for gcc-8.2.5 ############################### "
 echo "######## Whizard2 build: `date` ################### "
 echo "######## setting env parameters ##############"
 
-install_dir=${HOME}/Tutorial/sw/x86_64_gcc82_centos7/whizard2
-setup_file=${install_dir}/init_whizard2.sh
 version=2.8.5
+install_dir=${HOME}/Tutorial/sw/x86_64_gcc82_centos7
+setup_file=${install_dir}/whizard2/${version}/init_whizard2.sh
 ncores=8      # Number of cores for compiling
 #
 # Create script to define whizard2 parameters
@@ -25,7 +25,8 @@ export LD_LIBRARY_PATH=\${PYTHONDIR}/lib:\${LD_LIBRARY_PATH}
 
 export OCAML_DIR=${install_dir}/ocaml/4.05.0
 export HEPMC_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96/HepMC/2.06.09/x86_64-centos7-gcc8-opt
-export LCIO_DIR=${install_dir}/lcio/v02-16-01
+# export LCIO_DIR=${install_dir}/lcio/v02-16-01
+export LCIO_DIR=/cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7/v02-02/lcio/v02-15-01
 
 export LD_LIBRARY_PATH=\$LCIO_DIR/lib:\$HEPMC_DIR/lib:\${OCAML_DIR}/lib:\$LD_LIBRARY_PATH
 export PATH=\$LCIO_DIR/bin:\${OCAML_DIR}/bin:\$PATH
@@ -54,12 +55,12 @@ cd whizard-${version}
 echo "Configure in progress"
 ./configure --enable-fastjet \
            --with-fastjet=/cvmfs/sft.cern.ch/lcg/releases/LCG_96/fastjet/3.3.2/x86_64-centos7-gcc8-opt \
-           --prefix=${install_dir}/${version} \
-           --datadir=${install_dir}/${version}/share \
+           --prefix=${install_dir}/whizard2/${version} \
+           --datadir=${install_dir}/whizard2/${version}/share \
+           --docdir=${install_dir}/whizard2/${version}/doc/whizard \
            --disable-lhapdf \
            --enable-fc-debug \
            F77=gfortran  FC=gfortran CC=gcc CXX=g++
-
 
 echo "make in progress"
 make -j ${ncores}
