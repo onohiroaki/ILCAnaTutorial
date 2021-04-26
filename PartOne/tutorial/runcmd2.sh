4e0d9da2b4a89d502b62c8eab112257a4d09c4c1
akiyamiyamoto <akiya.miyamoto@kek.jp>
2021-04-26 10:50:53 +0900

    Correct error

--- PartOne/tutorial/runcmd2.sh
+++ PartOne/tutorial/runcmd2.sh
@@ -3,7 +3,8 @@
 # Creat link or downloas files for tutorial
 
 slachttp="http://osggridftp02.slac.stanford.edu:8080/sdf/group/lcddata/ilc"
-kekdisk="/group/ilc/grid/storm2"
+kekdisk="/group/ilc/grid/storm"
+kyusyudir="/home/miyamotoi/data"
 basedir="prod/ilc/mc-dbd/generated/250-TDR_ws"
 
 curdir=`pwd`
@@ -12,10 +13,12 @@ mkdir -p ${HOME}/Tutorial/data
 cd ${HOME}/Tutorial/data 
 while read infile ; do 
   datafile=${kekdisk}/${basedir}/${infile}
-  if [ ! -e ${datafile} ] ; then
-    wget ${slachttp}/${basedir}/${infile}
-  else 
+  if [ -e ${kyusyudir}/${infile} ] ; then 
+    ln -sfv ${kyusyudir}/${infile} . 
+  elif [ -e ${datafile} ] ; then 
     ln -sfv ${datafile} .
+  else
+    wget ${slachttp}/${basedir}/${infile}
   fi
 done <<EOF
 higgs/E250-TDR_ws.Pe2e2h.Gwhizard-1_95.eR.pL.I106480.001.stdhep
